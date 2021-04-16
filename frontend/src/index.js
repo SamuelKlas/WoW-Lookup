3c07b5ff891385cd427ad82060dbf6f3082a23d1
klassamu <klassamu@fel.cvut.cz>
2021-04-16 02:20:58 +0200

    add pvp and mythicPlus data fetching

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,25 +6,48 @@ import './index.css';
 class Character extends React.Component{
     constructor(props){
         super(props)
-        this.state = {charUrl : props.charUrl,loaded : false}
         this.fetchDataDemo = this.fetchDataDemo.bind(this)
     }
 
 
     async fetchDataDemo(){
-        let response = await fetch('http://localhost:8080/backend/eu/outland/trillethree/pvp/2v2')
-
-        let data = await response.json()
+        let data = await this.fetchMythicPlusData()
         console.log(data)
-        let response2 = await fetch('http://localhost:8080/backend/eu/outland/trillethree/pvp/3v3')
-
-        let data2 = await response2.json()
-        console.log(data2)
-        return data
     }
 
-    componentDidMount() {
+    async fetchMythicPlusData(){
+        let dungeonData = await (await fetch(this.props.baseUrl + '/mythicPlus/raiderIO')).json()
+        let bestRuns = dungeonData.mythic_plus_best_runs;
+        let rioScore = bestRuns.map(run =>run.score).reduce((a, b) => a + b, 0)
 
+        let filteredData = {
+            bestRuns : bestRuns,
+            score : rioScore,
+        }
+        
+        return filteredData;
+    }
+
+    async componentDidMount() {
+        let filteredData = await this.fetchPvpData()
+        this.setState({pvpData : filteredData})
+    }
+
+    async fetchPvpData(){
+        let pvp2v2Data = await (await fetch(this.props.baseUrl + '/pvp/2v2')).json()
+        let pvp3v3Data = await (await fetch(this.props.baseUrl + '/pvp/3v3')).json()
+        let pvpRbgData = await (await fetch(this.props.baseUrl + '/pvp/rbg')).json()
+
+        let filteredData = Array.of(pvp2v2Data,pvp3v3Data,pvpRbgData).map(data =>
+            ({
+                bracket : data.bracket.type,
+                rating : data.rating,
+                seasonStatistics : data.season_match_statistics,
+                weeklyStatistics : data.weekly_match_statistics
+
+            })
+        )
+        return filteredData
     }
 
     render(){
@@ -37,7 +60,7 @@ class Character extends React.Component{
 
 
 ReactDOM.render(
-    <Character charUrl = "https://eu.api.blizzard.com/profile/wow/character/outland/trillethree/equipment?namespace=profile-eu&locale=en_GB" />
+    <Character baseUrl = "http://localhost:8080/backend/eu/drakthul/adaldo" />
 ,
   document.getElementById('root')
 );
