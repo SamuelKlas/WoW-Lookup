6f83958e6d70ccc60cdc440c4a425094ee92d506
klassamu <klassamu@fel.cvut.cz>
2021-06-06 17:06:09 +0200

    add character level

--- frontend/src/Summary.js
+++ frontend/src/Summary.js
@@ -1,36 +1,31 @@
 import React, {Component} from 'react';
-import "./pvpItem.css"
-import "./classColors.css"
+import "./styles.css"
+import "./colors.css"
 
 
 class Summary extends Component {
     render() {
         return (
-            <div className="equip">
+            <section className="equip" style={{
+                backgroundImage: `url(/factions/${this.props.data.faction.toLowerCase()}.jpg)`
+                , backgroundRepeat: "no-repeat", backgroundPosition: "50% 0"
+            }}>
                 <h2>{this.props.data.title}</h2>
                 <div>
                     <p>
                         {this.props.data.guild}
                     </p>
+                    <p>Level {this.props.data.level}</p>
                     <p>
                         <span className={this.props.data.faction.toLowerCase()}>{this.props.data.race + " "}</span>
                         <span
-                            className={this.props.data.class.toLowerCase().replace(" ","")}>
+                            className={this.props.data.class.toLowerCase().replace(" ", "")}>
                             {this.props.data.spec + " " + this.props.data.class}
                         </span>
                     </p>
-
-
-                    {/*<div>*/}
-                    {/*    <img src={"/covenants/" + this.props.covenant + ".jpg"} alt=""/>*/}
-                    {/*    <ToolTip className="summaryTooltip" renown={this.props.data.renown}/>*/}
-                    {/*    <p>{this.props.covenant}</p>*/}
-
-                    {/*</div>*/}
-
                     <img className="backImage" src={this.props.mainUrl} alt=""/>
                 </div>
-            </div>
+            </section>
         );
     }
 }
